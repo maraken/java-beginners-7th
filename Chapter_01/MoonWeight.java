ee90fbb82436c4ed0df1dfe880b0bf62b5a2b8b6
maraken <mmarquez1981@gmail.com>
2017-10-12 02:05:06 -0500

    Complete Chapter 1 self test and add .gitignore

---
+++ Chapter_01/MoonWeight.java
@@ -0,0 +1,7 @@
+class MoonWeight{
+    public static void main(String args[]){
+        double weight = 265;
+
+        System.out.println("My Weight on the moon is " + weight * .17 + " lbs.");
+    }
+}
